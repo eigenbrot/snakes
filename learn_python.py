2bd5e8dc87b94b0877b7143e509bf32a335f2524
eigenbrot <eigenbrot@astro.wisc.edu>
2013-02-01 11:25:56 -0600

    commit test and update to learn_python

--- learn_python.py
+++ learn_python.py
@@ -17,9 +17,12 @@ import matplotlib.pyplot as plt # pyplot is a class within the matplotlib module
 
 # Ok now let's define a function to do some cool stuff. We'll start with pure
 # Python code and add in the cool science-related stuff in a bit
+#
+# One thing to say right away is that, unlike IDL, capitalization matters in
+# Python: variable != Variable
 
-def greatFunction(num, power, inverse=False): #keywords work just like IDL!
-    '''Another way to comment python code is like this. It's useful for
+def great_function(num, power, inverse=False): #keywords work just like IDL!
+    """Another way to comment python code is like this. It's useful for
     multi-line comments. 
 
     It is good pratice to put a docstring like this at the start of every 
@@ -27,7 +30,8 @@ def greatFunction(num, power, inverse=False): #keywords work just like IDL!
 
     This function takes in a number and raises it to some power. If inverse
     is True then the number will be raised to 1/power
-    '''
+
+    """
 
     # let's see an if/else statement 
     # NOTE: Whitespace in Python is Very Important. It is the only way to
@@ -60,3 +64,70 @@ x_sub = x[1:4] # another list that is [x[1],x[2],x[3]] NOTE the last slice
                # index is NOT included
 
 
+# Lists can contain any variable type you want, and even a mix of them.
+# Here's a fun and important fact: EVERYTHING in Python can be assigned a
+# variable. This means that lists can contain numbers, strings, characters,
+# functions, classes, modules, lists, etc. This is very exciting.
+#
+# This is a good time to talk about loops in Python, which can be quite
+# differnt from loops in other languages. In the most basic sense, a Python
+# loops iterates over the elements in a list. Here's a very basic example that
+# also shows off the versitility of lists:
+
+def loop_test():
+    """a simple function to show how Python loops work. Returns the contents
+    of a list
+
+    """
+    # define the list, let's put multiple types in it
+    l = [1,'yes',1e4,True,False]
+
+    # now loop over that list
+    for element in l:
+        print element
+
+    return
+
+# Ok, that's cool, but what if you want to loop in the more "traditional
+# sense? Easy! Use the range() function to generate a list and then loop over
+# it:
+
+def fib(n):
+    """generates the first n fibbboonnaacci (sp?) numbers while teach about
+    loops and lists! Any fibboonnnnnnnnaci program worth it's salt should be
+    recursive, but w/e.
+
+    Returns: a list containing the first n fibbonaciii (sp?) numbers and list
+    with the output of range(n)
+
+    """
+    
+    outlist = [0,1] # we have to cheat a little bit
+    countlist = [] # we'll use this one to illustrate how range() works
+    
+    for i in range(n-2): # the n-2 is because we start with the first 2
+                         # elements already
+        
+        countlist.append(i)
+        outlist.append(outlist[-1] + outlist[-2])
+
+    return outlist, countlist
+
+# what's up with that return statement? It's True! You can return as many
+# things as you want. There are few different ways to capture this output:
+
+fiblist, rangelist = fib(5)
+
+listtup = fib(5)
+
+# listtup is what we call a tuple (don't ask how to pronounce it) Tuples are a
+# lot like lists, but they defined with paranthesis rather than brackets, e.g.
+
+l = [1,2,3] # a list
+t = (1,2,3) # a tuple
+
+l[1] == t[1] # True!
+
+# There are some subtlties between lists and tuples that are not important
+# right now. In general you should be using lists beacuse they have more
+# features than tuples.
