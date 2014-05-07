fbf20efbd0f02c948e8640a8db755e359e2ccbbc
eigenbrot <eigenbrot@astro.wisc.edu>
2014-05-07 18:04:17 -0500

    Turned test into fitms(). This works on every aperture in a .ms file.

--- stellarFit.py
+++ stellarFit.py
@@ -8,6 +8,7 @@ from ppxf import ppxf
 import ppxf_util as pputil
 from scipy import ndimage
 from pyraf import iraf
+from matplotlib.backends.backend_pdf import PdfPages as PDF
 iraf.noao.onedspec()
 
 glob = glob.glob
@@ -44,115 +45,132 @@ def make_SSP_fits(input_file,output_fits):
 
     return
 
-def test(spectrum,template,line=10, order=5, cut=0.75):
-
-    data_hdu = pyfits.open(spectrum)
-    wave = data_hdu[0].header['CRVAL1'] + \
-        np.arange(data_hdu[0].data[line].size)*data_hdu[0].header['CDELT1']
-    galaxy = data_hdu[0].data[line][106:]
-    wave = wave[106:]
-
-    temp_hdu = pyfits.open(template)
-    twave = temp_hdu[1].header['CRVAL1'] + \
-        np.arange(temp_hdu[1].data.size)*temp_hdu[1].header['CDELT1']
-    template = temp_hdu[1].data
-
-    mask = np.where((twave > wave.min()) & (twave < wave.max()))
-    # twave = twave[mask]
-    # template = template[mask]
-
-    fig = plt.figure()
-    ax = fig.add_subplot(311)
-    ax.plot(wave,galaxy)
-    datafit = ADE.polyclip(wave,galaxy,order)
-    ax.plot(wave,datafit(wave))
-    galaxy /= datafit(wave)
-    ax2 = fig.add_subplot(312)
-#    ax2.plot(wave,galaxy)
-
-    # fig2 = plt.figure()
-    # ax3 = fig2.add_subplot(211)
-    # ax3.plot(twave,template)
-    # tmpfit = ADE.polyclip(twave,template,order)
-    # ax3.plot(twave,tmpfit(twave))
-    # template /= tmpfit(twave)
-    # ax4 = fig2.add_subplot(212)
-#    ax4.plot(twave,template)
+def logify(infits, outfits, w1='INDEF',w2='INDEF',
+           dw='INDEF',nw='INDEF',report=False):
     
-    print wave.shape, twave.shape
+    iraf.noao.onedspec.dispcor(infits,outfits,
+                               linearize=True,log=False,flux=True,
+                               dw=dw, w1=w1, w2=w2, nw=nw)
+    if report:
+        hdu = pyfits.open(outfits)[0]
+        dw = hdu.header['CDELT1']
+        nw = hdu.header['NAXIS1']
+        w1 = hdu.header['CRVAL1']
+        w2 = w1 + nw*dw
+
+    return w1, w2, dw, nw
+
+def prep_templates(template_fits, outputfits, dw):
+
+    thdu = pyfits.open(template_fits)[0]
+    tw1 = thdu.header['CRVAL1']
+    tn2 = thud.header['NAXIS1']
+    
+
+def fitms(spectrum,template, out_prefix, order=5, cut=0.75, pre=106):
+
+    pd = PDF(out_prefix+'.pdf')
+
+    #make the galaxy wavelength vector
+    data_hdu = pyfits.open(spectrum)
+    ddw = data_hdu[0].header['CDELT1']
+    wave = data_hdu[0].header['CRVAL1'] + \
+        np.arange(data_hdu[0].shape[1])*ddw
+    wave = wave[pre:]
+    lamRange1 = np.array([wave.min(),wave.max()])
+    print data_hdu[0].shape
 
     c = 299792.458
     FWHM_gal = 0.95 #AA
     FWHM_tmp = 0.55 #AA
     FWHM_diff = np.sqrt(FWHM_gal**2 - FWHM_tmp**2)
-    sigma = FWHM_diff/2.355/data_hdu[0].header['CDELT1']
+    sigma = FWHM_diff/2.355/ddw
 
-    lamRange1 = np.array([wave.min(),wave.max()])
-    loggalaxy, logLam1, velscale = pputil.log_rebin(lamRange1,galaxy)
-
+    temp_hdu = pyfits.open(template)
+    twave = temp_hdu[1].header['CRVAL1'] + \
+        np.arange(temp_hdu[1].data.size)*temp_hdu[1].header['CDELT1']
+    template = temp_hdu[1].data
     lamRange2 = np.array([twave.min(),twave.max()])
     template = ndimage.gaussian_filter1d(template,sigma)
-#    ax4.plot(twave,template)
-    logtmp, logLam2, velscale_tmp = pputil.log_rebin(lamRange2,
-                                                     template,
-                                                     velscale=velscale)
-    templates = np.empty((logtmp.size,len(temp_hdu)-1))
 
-    for i in range(len(temp_hdu)-1):
-        hdu = temp_hdu[i+1]
-        twave = temp_hdu[1].header['CRVAL1'] + \
-        np.arange(temp_hdu[1].data.size)*temp_hdu[1].header['CDELT1']
-        td = hdu.data
-        tfit = ADE.polyclip(twave,td,order)
-        td /= tfit(twave)
-        td = ndimage.gaussian_filter1d(td,sigma)
-        logtd, logLam2, velscale = pputil.log_rebin(lamRange2,td,
-                                                    velscale=velscale)
-        templates[:,i] = logtd/np.median(logtd)
+    bestfits = np.empty((1,wave.size))
+
+    for i in range(data_hdu[0].data.shape[0]):
+
+        fig = plt.figure()
+
+        galaxy = data_hdu[0].data[i][pre:]
+        datafit = ADE.polyclip(wave,galaxy,order)
+
+        ax = fig.add_subplot(211)
+        ax.plot(wave,galaxy)
+        ax.plot(wave,datafit(wave))
+        galaxy /= datafit(wave)
+
+        loggalaxy, logLam1, velscale = pputil.log_rebin(lamRange1,galaxy)
+
+        logtmp, logLam2, velscale_tmp = pputil.log_rebin(lamRange2,
+                                                         template,
+                                                         velscale=velscale)
+
+        templates = np.empty((logtmp.size,len(temp_hdu)-1))
+
+        for i in range(len(temp_hdu)-1):
+            hdu = temp_hdu[i+1]
+            twave = temp_hdu[1].header['CRVAL1'] + \
+                np.arange(temp_hdu[1].data.size)*temp_hdu[1].header['CDELT1']
+            td = hdu.data
+            tfit = ADE.polyclip(twave,td,order)
+            td /= tfit(twave)
+            td = ndimage.gaussian_filter1d(td,sigma)
+            logtd, logLam2, velscale = pputil.log_rebin(lamRange2,td,
+                                                        velscale=velscale)
+            templates[:,i] = logtd/np.median(logtd)
     
-    print templates.shape
+        print templates.shape
 
-    dv = (logLam2[0] - logLam1[0])*c
-    vel = c*1.008246
-#    goodpixels = pputil.determine_goodpixels(np.log(twave),lamRange,vel)
-#    goodpixels = np.where(loggalaxy > 0.8)[0]
-    start = [0,180.]
-#    template /= np.median(template)
-    loggalaxy /= np.median(loggalaxy)
-    goodpixels = np.where(np.abs(loggalaxy - np.mean(loggalaxy)) < \
-                              cut*np.std(loggalaxy))[0]
-    collection = collections.BrokenBarHCollection.span_where(
-        np.arange(loggalaxy.size),
-        ymin=0,ymax=4,
-        where=np.abs(loggalaxy - np.mean(loggalaxy)) >= cut*np.std(loggalaxy), 
-        facecolor='red',alpha=0.5)
-    ax2.plot(np.arange(loggalaxy.size),loggalaxy)
-    ax2.add_collection(collection)
+        dv = (logLam2[0] - logLam1[0])*c
+        vel = c*1.008246
+        start = [0,180.]
+        loggalaxy /= np.median(loggalaxy)
+        goodpixels = np.where(np.abs(loggalaxy - np.mean(loggalaxy)) < \
+                                  cut*np.std(loggalaxy))[0]
+       
+        collection = collections.BrokenBarHCollection.span_where(
+            np.arange(loggalaxy.size),
+            ymin=0,ymax=4,
+            where=np.abs(loggalaxy - np.mean(loggalaxy)) \
+                >= cut*np.std(loggalaxy), 
+            facecolor='red',alpha=0.5)
         
+        ax2 = fig.add_subplot(212)
+        ax2.plot(np.arange(loggalaxy.size),loggalaxy)
+        ax2.add_collection(collection)
+        
+        pp = ppxf(templates,loggalaxy, np.ones(loggalaxy.shape), 
+                  velscale,start, 
+                  vsyst=dv,plot=True, goodpixels=goodpixels)
 
-#    fig2.show()
+        print bestfits.shape, pp.bestfit.size
+        bestfits = np.vstack((bestfits,pp.bestfit))
 
-    pp = ppxf(templates,loggalaxy, np.ones(loggalaxy.shape), 
-              velscale,start, 
-              vsyst=dv,plot=True, goodpixels=goodpixels)
+        ax2.plot(np.arange(loggalaxy.size),pp.bestfit)
+        ax2.set_ylim(0.8,1.2)
+        pd.savefig(fig)
 
-    # pyfits.PrimaryHDU(templates.T).writeto('templates.fits',
-    #                                      clobber=True)
-    bfh = pyfits.PrimaryHDU(np.array(pp.bestfit[np.newaxis,:],dtype=np.float32))
-    bfh.header.update('CDELT1',data_hdu[0].header['CDELT1'])
+    bestfits = bestfits[1:]
+    pd.close()
+    bfh = pyfits.PrimaryHDU(np.array(bestfits,dtype=np.float32))
+    bfh.header.update('CDELT1',ddw)
     bfh.header.update('CRVAL1',wave.min())
     bfh.header.update('CRPIX1',1)
     bfh.header.update('CTYPE1','LINEAR')
-    bfh.writeto('bestfit.fits',clobber=True)
-    iraf.noao.onedspec.dispcor('bestfit.fits','bestfit_lin.fits',
+    bfh.writeto(out_prefix+'.fits',clobber=True)
+    iraf.noao.onedspec.dispcor(out_prefix+'.fits',out_prefix+'_lin.fits',
                                linearize=True,log=False,flux=False,
-                               dw=data_hdu[0].header['CDELT1'],
-                               w1=wave.min(),w2=wave.max())
-    bestfit = pyfits.open('bestfit_lin.fits')[0].data[0]
-    ax3 = fig.add_subplot(313)
-    ax3.plot(wave,galaxy,'--',color='k',alpha=0.5)
-    ax3.plot(wave,bestfit)
-    fig.show()
-                               
-    return pp, bestfit
+                               dw=ddw,w1=wave.min(),w2=wave.max(),nw='INDEF',
+                               samedis=True)
+
+
+    return
     
