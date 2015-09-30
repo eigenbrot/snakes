ec8381163cc4633b6f18913d9fddc9cd8d8a8814
eigenbrot <eigenbrot@astro.wisc.edu>
2015-09-30 10:52:20 -0500

    Increased richness of plot information by 1000%

--- Gradstats.py
+++ Gradstats.py
@@ -5,13 +5,15 @@ plt.ioff()
 from glob import glob
 import re
 import os
+import sys
+import time
 
 wavemin = 4500.
 wavemax = 5500.
 
-def go(prefix='NGC_891'):
+def go(pointing='*',prefix='NGC_891'):
 
-    skylist = glob('{}*otdcs*ms_lin.fits'.format(prefix))
+    skylist = glob('{}_P{}*otdcs*ms_lin.fits'.format(prefix,pointing))
     skylist.sort()
     sslist = [s.replace('ms_lin','ms_s_lin') for s in skylist]
     print skylist
@@ -20,7 +22,7 @@ def go(prefix='NGC_891'):
     p = re.compile('\d_(\d{3})_')
     fnum = np.array([int(p.search(i).group(1)) for i in skylist])
     
-    mjd = np.array([float(pyfits.open(s)[0].header['MJD-OBS']) for s in skylist])
+    mjd = np.array([float(pyfits.open(s)[0].header['JD']) for s in skylist])
     mjd -= np.min(mjd)
     mjd *= 24.
 
@@ -30,18 +32,26 @@ def go(prefix='NGC_891'):
 
     fig = plt.figure()
     skax = fig.add_subplot(311)
-    skax.set_ylabel('Sky flux')
+    skax.set_ylabel('$\\frac{B_S}{<B_S>_{\mathrm{night}}}$',fontsize=14)
     
     soax = fig.add_subplot(312)
-    soax.set_ylabel('f - s/<f - s>')
+    soax.set_ylabel('$\\frac{F - S}{<F - S>_{\mathrm{night}}}$',fontsize=14)
 
     rmax = fig.add_subplot(313)
     rmax.set_xlabel('Hours since first exposure')
-    rmax.set_ylabel('rms(sky - <sky>)')
+    rmax.set_ylabel('rms($S - <S>_{\mathrm{size}}$)',fontsize=10)
 
-    skax.errorbar(mjd, sky[0], yerr=sky[1],ls='',marker='o')
-    soax.errorbar(mjd, source_sky[0], yerr=source_sky[1],ls='',marker='o')
-    rmax.errorbar(mjd, rms[0], yerr=rms[1],ls='',marker='o')
+    skax.errorbar(mjd, sky[0], yerr=sky[1],ls='',marker='o',color='k')
+    soax.errorbar(mjd, source_sky[0], yerr=source_sky[1],ls='',marker='o',color='k')
+    rmax.errorbar(mjd, rms[0], yerr=rms[1],ls='',marker='o',color='k')
+
+    for i in range(mjd.size):
+        skax.text(mjd[i]+0.08, sky[0,i],'{:4.2f}'.format(sky[0,i]),
+                  va='center',fontsize=7)
+        soax.text(mjd[i]+0.08, source_sky[0,i],'{:4.2f}'.format(source_sky[0,i]),
+                  va='center',fontsize=7)
+        rmax.text(mjd[i]+0.08, rms[0,i],'{:4.2f}'.format(rms[0,i]),
+                  va='center',fontsize=7)
 
     skax.set_xticklabels([])
     soax.set_xticklabels([])
@@ -56,11 +66,38 @@ def go(prefix='NGC_891'):
     rmax.set_xlim(skax.get_xlim())
     utcax.set_xlim(skax.get_xlim())
 
-    skax.set_ylim(skax.get_ylim()*np.array([0.9,1.]))
-    soax.set_ylim(soax.get_ylim()*np.array([0.9,1.1]))
+    skax.set_ylim(skax.get_ylim()*np.array([0.999,1.]))
+    soax.set_ylim(soax.get_ylim()*np.array([0.999,1.1]))
     rmax.set_ylim(rmax.get_ylim()*np.array([1.,1.1]))
 
     fig.subplots_adjust(hspace=0.0001)
+
+    skax.text(0.1,0.9,'Error bars are $\sigma$\nacross all fibers',
+              transform=skax.transAxes, va='top',
+              fontsize=9)
+    soax.text(0.1,0.9,'Error bars are $\sigma$\nacross all fibers',
+              transform=soax.transAxes, va='top',
+              fontsize=9)
+    rmax.text(0.1,0.9,'Error bars are $\sigma$\nof RMS across each\nfiber size',
+              transform=rmax.transAxes, va='top',
+              fontsize=9)
+
+    skax.text(-0.1,1.3,
+              '$B_S$ = Sky surface brightness',
+              transform=skax.transAxes, va='top',
+              fontsize=7)
+    skax.text(-0.1,1.23,
+              '$F$ = Galaxy flux',
+              transform=skax.transAxes, va='top',
+              fontsize=7)
+    skax.text(-0.1,1.16,
+              '$S$ = Sky flux',
+              transform=skax.transAxes, va='top',
+              fontsize=7)
+
+    skax.text(0.85,1.3,'{}_P{}\n{}'.format(prefix,pointing,time.asctime()),
+              transform=skax.transAxes, va='top',
+              fontsize=7)
 
     return fig
 
@@ -76,7 +113,7 @@ def get_sky(skylist):
 
     skidx = np.array([1,2,18,19,20,31,32,43,44,53,54,62,63,71,79,87,88,95,102,109]) - 1
     print 'Sky:'
-    output = []
+    tmp = []
     for frame in skylist:
         print '\t', frame
         wave, data = openfits(frame)
@@ -89,11 +126,14 @@ def get_sky(skylist):
         d[12:16] /= 5.
         d[16:20] /= 6.
         med = np.median(d,axis=1)
-        sky = np.mean(med)
-        skystd = np.std(med)
-        output.append(np.array([sky,skystd]))
+        tmp.append(med)
+        
+    d = np.vstack(tmp)
+    skym = np.mean(d,axis=0)
+    sky = np.mean(d/skym,axis=1)
+    skystd = np.std(d/skym,axis=1)
 
-    return np.array(output).T
+    return np.array([sky,skystd])
         
         
 def get_source(inlist):
@@ -108,7 +148,6 @@ def get_source(inlist):
         tmp.append(d)
 
     d = np.vstack(tmp)
-    print d.shape
 
     m = np.mean(d,axis=0)
     source = np.mean(d/m,axis=1)
@@ -142,8 +181,15 @@ def get_rms(skylist):
 def main():
 
     cwd = os.getcwd()
-    plot_name = "{}-{}_stats.png".format(*cwd.split('/')[-2:])
-    f = go()
+    try:
+        p = sys.argv[1]
+        split = cwd.split('/')
+        plot_name = "{}-P{}-{}_stats.png".format(split[-2],p,split[-1])
+    except IndexError:
+        p = '*'
+        plot_name = "{}-{}_stats.png".format(*cwd.split('/')[-2:])
+
+    f = go(pointing=p)
     f.savefig(plot_name)
 
     return 0
