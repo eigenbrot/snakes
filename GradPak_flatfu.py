A few important fixes to flat scaling.
1) We now use the 3'' fibers, 4'' proved to often be saturated. This
should be a user option in the future

2) No more separate scaling step. All of that happens as part of the
shutter correction, which makes a lot of sense (why didn't we do this
before?)

GradPak_flatfu.py
@@ -35,7 +35,7 @@
 #      v1.1 - A. Eigenbrot Dec. 2014
 #      v2.0 - A. Eigenbrot Aug. 2015
 #               Corrected implimentation of fitflat+
-#      v2.1 - A. Eigenbrot Sep. 2015
+#      v2.1 - A. Eigenbrot Oct. 2015
 #               Added shutter lag correction
 #
 ####################################################
@@ -82,8 +82,8 @@ def scale_images(hdulist):
 
 def scale_spectra(imagelist):
 
-    fiber1 = 44
-    fiber2 = 62
+    fiber1 = 20
+    fiber2 = 43
 
     hdulist = [pyfits.open(image)[0] for image in imagelist]
     means = [np.mean(h.data[fiber1 - 1:fiber2 - 1,:]) for h in hdulist]
@@ -100,12 +100,14 @@ def scale_spectra(imagelist):
 
 def shutter_correction(imagelist):
 
-    fiber1 = 44
-    fiber2 = 62
+    fiber1 = 20
+    fiber2 = 43
 
     hdulist = [pyfits.open(image)[0] for image in imagelist]
     avgs = [np.mean(h.data[fiber1 - 1:fiber2 - 1,:],axis=0) for h in hdulist]
     corrections = avgs[0]/np.array(avgs)
+
+    pyfits.PrimaryHDU(corrections,hdulist[0].header).writeto('corrections.ms.fits',clobber=True)
 
     outputnames = ['{}_shut.ms.fits'.format(image.split('.ms.fits')[0]) for image in imagelist]
     print 'Correcting for shutter lag...'
@@ -420,7 +422,7 @@ def main():
         msl, scales = initial_run(msl, traceflat, throughput)
     outstring = get_scrunch(flat_list[0],msl[0])
     mean_scale(msl,scales)
-    msl = scale_spectra(msl)
+    #msl = scale_spectra(msl)
     msl = shutter_correction(msl)
     master = stitch_flats(msl,pivot_list,outstring)
     if fitflat: