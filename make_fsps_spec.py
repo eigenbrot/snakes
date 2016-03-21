699fbbf44be8f7768021fa99c14f3eb46823ff9b
eigenbrot <eigenbrot@astro.wisc.edu>
2016-03-21 11:57:34 -0500

    Fixed weird bug(?) that caused all SSPs to be the same age.
    I'm still not sure I totally understand how sf_start and tage work
    together.

--- make_fsps_spec.py
+++ make_fsps_spec.py
@@ -3,6 +3,7 @@ import fsps
 
 def make_single_age(age, Z=1.0, wavemin=3000, wavemax=9000):
 
+    t0 = 13.8
     z_arr = np.array([0.0002, 0.0003, 0.0004, 
                       0.0005, 0.0006, 0.0008, 
                       0.0010, 0.0012, 0.0016, 
@@ -13,7 +14,7 @@ def make_single_age(age, Z=1.0, wavemin=3000, wavemax=9000):
 
     
     zidx = np.argmin(np.abs(Z - z_arr))
-    print '\t',zidx
+    print '\t',zidx+1
     if zidx.size == 0:
         print 'Could not find Z = {}'.format(Z)
         return
@@ -21,14 +22,15 @@ def make_single_age(age, Z=1.0, wavemin=3000, wavemax=9000):
     sp = fsps.StellarPopulation(add_stellar_remnants=False, 
                                 add_dust_emission=False, 
                                 add_agb_dust_model=False, 
-                                dust_type=0, 
+                                dust_type=0,
+                                dust1=0,
                                 dust_index=0, 
                                 imf_type=1, 
-                                sf_start=age, 
+                                sf_start=0,
                                 zmet=zidx, 
                                 sfh=0)
 
-    wave, spec = sp.get_spectrum(tage=13.8, peraa=True)
+    wave, spec = sp.get_spectrum(tage=age, peraa=True)
 
     idx = np.where((wave > wavemin) & (wave < wavemax))[0]
 
