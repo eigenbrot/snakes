ef0548b4e305d3c897e624cfece5a88815b3523d
eigenbrot <eigenbrot@astro.wisc.edu>
2016-04-06 16:38:17 -0500

    A few updates to KegPrint to get it ready for mass distribution :)
    o Separate log files for each computer
    o Waiting now happens within python
    o Automatic initialization of log files

--- KegPrint.py
+++ KegPrint.py
@@ -3,18 +3,25 @@
 import subprocess
 import os
 import time
+import socket
+
+logfile = os.path.expanduser('~/.kp_{}.log'.format(socket.gethostname()))
 
 def check_log():
     
     last_line = subprocess.check_output(['tail','-1','/var/log/cups/page_log'])
     printer = last_line.split(' ')[0]
-    printID = int(last_line.split(' ')[1])
-
+    try:
+        printID = int(last_line.split(' ')[1])
+    except ValueError:
+        #The Cupsd.conf PageLogFormat is a little weird, we'll try one alternative
+        printID = int(last_line.split(' ')[2])
+        
     return printer, printID
 
 def check_new(printID):
 
-    with open(os.path.expanduser('~/.bk.log'),'r') as f:
+    with open(logfile,'r') as f:
         lines = f.readlines()
         lastID = int(lines[-1].split('\n')[0])
     
@@ -31,7 +38,7 @@ def do_print(printer):
 
 def update_ID(printID):
 
-    with open(os.path.expanduser('~/.bk.log'),'a') as f:
+    with open(logfile,'a') as f:
         f.write('{}\n'.format(printID+1))
 
     return
@@ -40,11 +47,21 @@ def main():
 
     printer, printID = check_log()
 
-    if check_new(printID):
-        do_print(printer)
-        update_ID(printID)
-        print "printed something new at {}".format(time.asctime())
+    if not os.path.exists(logfile):
+        print "Initializing logfile"
+        with open(logfile,'w') as f:
+            f.write('{}\n'.format(printID))
+            
+    print "Waiting..."
+    while True:
 
+        if check_new(printID):
+            do_print(printer)
+            update_ID(printID)
+            print "printed something new at {}".format(time.asctime())
+        
+        time.sleep(10)
+        
     return
 
 if __name__ == '__main__':
