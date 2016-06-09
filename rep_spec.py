fbf5edf833048d8a9dd0b9af1cf3f0cf3950cbd3
eigenbrot <eigenbrot@astro.wisc.edu>
2016-06-09 17:28:43 -0500

    Added functions to make cut grids, like in D4000_indices

--- rep_spec.py
+++ rep_spec.py
@@ -1,6 +1,7 @@
 import numpy as np
 import re
 import pyfits
+import plot_simple as ps
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages as PDF
 plt.ioff()
@@ -85,6 +86,116 @@ def cut_pointing(pointing, zcut=0.5):
     high_d = np.sum(data[zhidx,:]*SN[zhidx,None], axis=0)/np.sum(SN[zhidx])
 
     return wave, low_d, high_d
+
+def get_all_data(exclude=exclude):
+    
+    biglist = []
+    bigr = []
+    bigz = []
+    bigSN = []
+    ss = re.compile(r'(\d+): \[(.*)\], SNR: (\d+\.\d+)')    
+    
+    for p in range(6):
+        loc = 'NGC_891_P{}_bin30_locations.dat'.format(p+1)
+        log = 'NGC_891_P{}_bin30.log'.format(p+1)
+        dfile = 'NGC_891_P{}_bin30.msoz.fits'.format(p+1)
+        print loc, log, dfile
+        r, z = np.loadtxt(loc, usecols=(4,5), unpack=True)
+        z = np.abs(z)
+        r = np.abs(r)
+
+        SN = np.zeros(z.size)
+        with open(log,'r') as lff:
+            lf = lff.readlines()
+        for i, l in enumerate(lf):
+            grp = ss.search(l).groups()
+            SN[i] = float(grp[2])
+        
+        hdu = pyfits.open(dfile)[0]
+        data = hdu.data
+
+        exar = np.array(exclude[p]) - 1
+        r = np.delete(r, exar)
+        z = np.delete(z, exar)
+        SN = np.delete(SN, exar)
+        data = np.delete(data, exar, axis=0)
+        
+        biglist.append(data)
+        bigr.append(r)
+        bigz.append(z)
+        bigSN.append(SN)
+
+    header = hdu.header    
+    cdelt = header['CDELT1']
+    crpix = header['CRPIX1']
+    crval = header['CRVAL1']
+
+    wave = (np.arange(data.shape[1]) + crpix-1)*cdelt + crval
+
+    return wave, np.hstack(bigr), np.hstack(bigz), np.hstack(bigSN), np.vstack(biglist)
+
+def plot_cut_all(wave, r, z, sn, data, ax = None, zcut=[0,2.7], rcut=[0,11]):
+    
+    if ax is None:
+        ax = plt.figure().add_subplot(111)
+        ax.set_xlabel('Wavelength [$\AA$]')
+        ax.set_ylabel('Normalized Flux')
+
+    idx = np.where((z >= zcut[0]) & (z < zcut[1])
+                   & (r >= rcut[0]) & (r < rcut[1]))[0]
+
+    avg = np.nansum(data[idx,:]*sn[idx,None],axis=0)/np.nansum(sn[idx])
+    
+    avg /= avg[810] #Approx 5500
+
+    ax.plot(wave, avg, 'k')
+
+    return ax, avg
+
+def plot_cut_grid(output, zcuts=[0.4], rcuts=[3,8], exclude=exclude):
+
+    fig = plt.figure()
+    lax = fig.add_subplot(111)
+    lax.spines['top'].set_visible(False)
+    lax.spines['right'].set_visible(False)
+    lax.spines['bottom'].set_visible(False)
+    lax.spines['left'].set_visible(False)   
+    lax.set_xticklabels([])
+    lax.set_yticklabels([])
+    lax.set_xlabel('Wavelength [$\AA$]')
+    lax.set_ylabel('Normalized Flux')
+    lax.tick_params(axis='both',pad=20,length=0)
+
+    bigz = [0] + zcuts + [2.6]
+    bigr = [0] + rcuts + [11]
+
+    w, r, z, sn, data = get_all_data(exclude=exclude)
+
+    i = 1
+    for zz in range(len(zcuts) + 1):
+        zc = [bigz[-zz-2], bigz[-zz-1]]
+        for rr in range(len(rcuts) + 1):
+            rc = [bigr[rr], bigr[rr+1]]
+            print zc, rc
+            ax = fig.add_subplot(len(zcuts)+1,len(rcuts)+1,i)
+            plot_cut_all(w,r,z,sn,data,zcut=zc,rcut=rc,ax=ax)
+
+            ax.text(4000,1.5,'${}\leq |z| <{}$ kpc\n${}\leq |r| <{}$ kpc'.format(*(zc+rc)),ha='left',va='center')
+            ax.set_ylim(0,1.9)
+            ax.set_xlim(3600,5900)
+            if i < 4:
+                ax.set_xticklabels([])
+            if i % 3 != 1:
+                ax.set_yticklabels([])
+            i += 1
+
+    fig.subplots_adjust(hspace=0.00001,wspace=0.0001)
+    
+    pp = PDF(output)
+    pp.savefig(fig)
+    pp.close() 
+    
+    return
 
 def plot_pointing(pointing, zcut=0.5, ax = None):
 
