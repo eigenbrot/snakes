23669f08402a7dbefda5f690ae1211f333a18a7e
eigenbrot <eigenbrot@astro.wisc.edu>
2016-07-21 17:07:21 -0500

    Add function to combine systematic and fitting error.
    This might not be the correct (physically) thing to do.

--- final_plots.py
+++ final_plots.py
@@ -5,7 +5,36 @@ from matplotlib.backends.backend_pdf import PdfPages as PDF
 
 exclude = [[5, 34], [1, 2, 35], [59], [2, 8], [1, 2, 3, 27, 28, 29,5], [35, 36, 38]]
 
-def val_pos_3panel(output, field='MLWA', colorfield='TAUV', basedir='.', exclude=exclude, rtrue=True):
+def combine_err():
+
+    for p in range(6):
+
+        basename = 'NGC_891_P{}_bin30_allz2'.format(p+1)
+        print basename
+        coefs = pyfits.open(basename+'.coef.fits')[1].data
+        fiterr = pyfits.open(basename+'.fiterr.fits')[1].data
+        syserr = pyfits.open(basename+'.syserr.fits')[1].data
+
+        numap = coefs.shape[0]
+        output = np.zeros(numap, dtype=[('MLWA','>f8'), ('dMLWA', '>f8'),
+                                        ('MLWZ','>f8'), ('dMLWZ', '>f8'),
+                                        ('TAUV','>f8'), ('dTAUV', '>f8')])
+
+        for i in range(numap):
+            output['MLWA'][i] = coefs['MLWA'][i]
+            output['MLWZ'][i] = coefs['MLWZ'][i]
+            output['TAUV'][i] = coefs['TAUV'][i]
+            output['dMLWA'][i] = np.sqrt(fiterr['dMLWA'][i]**2 + syserr['dMLWA'][i]**2)
+            output['dMLWZ'][i] = fiterr['dMLWZ'][i]
+            output['dTAUV'][i] = fiterr['dTAUV'][i]
+
+        outname = basename + '.final.fits'
+        pyfits.BinTableHDU(output).writeto(outname, clobber=True)
+
+    return
+
+def val_pos_3panel(output, field='MLWA', colorfield='TAUV', basedir='.', 
+                   exclude=exclude, rtrue=True):
 
     fig = plt.figure(figsize=(8,8))
     ax1 = fig.add_subplot(221)
@@ -67,7 +96,7 @@ def val_pos_3panel(output, field='MLWA', colorfield='TAUV', basedir='.', exclude
     return
 
 def val_pos_2panel(output, field, errfield=None, basedir='.', exclude=exclude, rtrue=True,
-                   label=None, plotfid=True):
+                   label=None, plotfid=True, suffix='final'):
 
     fig = plt.figure()
     ax1 = fig.add_subplot(121)
@@ -83,7 +112,8 @@ def val_pos_2panel(output, field, errfield=None, basedir='.', exclude=exclude, r
         if rtrue:
             rphi = '{}/NGC_891_P{}_bin30_rphi.dat'.format(basedir,p+1)
             r = np.loadtxt(rphi, usecols=(1,), unpack=True)
-        coef = '{}/NGC_891_P{}_bin30_allz2.coef.fits'.format(basedir,p+1)
+        coef = '{}/NGC_891_P{}_bin30_allz2.{}.fits'.format(basedir,p+1,suffix)
+        print coef
         c = pyfits.open(coef)[1].data
         
         exarr = np.array(exclude[p]) - 1
@@ -154,3 +184,51 @@ def val_pos_2panel(output, field, errfield=None, basedir='.', exclude=exclude, r
 
     return
 
+def compare_weights(field, errfield, output, bins=10):
+
+    bigf0_list = []
+    bige0_list = []
+    bigf1_list = []
+    bige1_list = []
+    for p in range(6):
+
+        basename = 'NGC_891_P{}_bin30_allz2.fiterr.fits'.format(p+1)
+        p0 = pyfits.open(basename)[1].data
+        p1 = pyfits.open('IRAF_weight/'+basename)[1].data
+
+        bigf0_list.append(p0[field])
+        bige0_list.append(p0[errfield])
+        bigf1_list.append(p1[field])
+        bige1_list.append(p1[errfield])
+
+    bigf0 = np.hstack(bigf0_list)
+    bige0 = np.hstack(bige0_list)
+    bigf1 = np.hstack(bigf1_list)
+    bige1 = np.hstack(bige1_list)
+
+    ax1 = plt.figure().add_subplot(111)
+    ax1.set_xlabel('p=0')
+    ax1.set_ylabel('p=1')
+    ax1.set_title(field)
+
+    ax2 = plt.figure().add_subplot(111)
+    ax2.set_xlabel(errfield)
+    ax2.set_ylabel('N')
+        
+    pe0 = bige0[np.isfinite(bige0)]
+    pe1 = bige1[np.isfinite(bige1)]
+        
+    ax1.scatter(bigf0,bigf1,c='k')
+    ax1.plot(bigf0,bigf0,color='k',ls=':',alpha=0.5)
+    ax2.hist(pe0,bins=bins,color='b',alpha=0.5,histtype='stepfilled',label='p=0')
+    ax2.hist(pe1,bins=bins,color='r',alpha=0.5,histtype='stepfilled',label='p=1')
+    ax2.legend(loc=0)
+
+    pp = PDF(output)
+    pp.savefig(ax1.figure)
+    pp.savefig(ax2.figure)
+    pp.close()
+    plt.close(ax1.figure)
+    plt.close(ax2.figure)
+
+    return
