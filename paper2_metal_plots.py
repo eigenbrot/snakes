5b38a9d6c3b638d8a8deb0d1ba39c2f4e5311531
eigenbrot <eigenbrot@astro.wisc.edu>
2016-11-14 15:46:11 -0600

    Added ability to color-code by height

--- paper2_metal_plots.py
+++ paper2_metal_plots.py
@@ -11,7 +11,7 @@ pap_ex = [[2, 5, 34],
           [1, 2, 3, 11, 13, 19, 27, 28, 29, 5], 
           [35, 36, 38]]
 
-def tau_Z(output,color=False, vmax=20, exclude=pap_ex, 
+def tau_Z(output, color=False, vmax=20, exclude=pap_ex, height=False,
           mass=False, mass_age=False, pivot_age=6, show_fit=True):
 
     basedir = '/d/monk/eigenbrot/WIYN/14B-0456/anal/final_results'
@@ -48,9 +48,16 @@ def tau_Z(output,color=False, vmax=20, exclude=pap_ex,
 
         if color:
             rc('figure.subplot', right=0.99)
-            rphi = '{}/NGC_891_P{}_bin30_rphi.dat'.format(basedir,p+1)
-            rr = np.loadtxt(rphi, usecols=(1,), unpack=True)
-            rlist.append(np.delete(rr,exarr))
+            if height:
+                loc = '{}/NGC_891_P{}_bin30_locations.dat'.format(basedir,p+1)
+                z = np.loadtxt(loc, usecols=(5,), unpack=True)
+                clabel = r'$|z|$'
+                rlist.append(np.delete(z, exarr))
+            else:
+                rphi = '{}/NGC_891_P{}_bin30_rphi.dat'.format(basedir,p+1)
+                rr = np.loadtxt(rphi, usecols=(1,), unpack=True)
+                clabel = r'$r$'
+                rlist.append(np.delete(rr,exarr))
         
     Z = np.hstack(Zlist)
     t = np.hstack(tlist)
@@ -86,7 +93,7 @@ def tau_Z(output,color=False, vmax=20, exclude=pap_ex,
         r = np.hstack(rlist)
         scat = ax.scatter(t,Z,c=r,s=45,linewidth=0, cmap=plt.cm.gnuplot2, vmin=0, vmax=vmax)
         cb = fig.colorbar(scat)
-        cb.set_label(r'$r$')
+        cb.set_label(clabel)
     else:
         ax.scatter(t,Z,c='k',s=45,linewidth=0)
 
@@ -185,7 +192,7 @@ def agemass_diff(output, exclude=pap_ex, vmax=20):
 
     return
 
-def mass_diff(output, exclude=pap_ex, vmax=20, age_weight='L'):
+def mass_diff(output, exclude=pap_ex, vmax=20, age_weight='L', height=False):
 
     
     basedir = '/d/monk/eigenbrot/WIYN/14B-0456/anal/final_results'
@@ -204,9 +211,16 @@ def mass_diff(output, exclude=pap_ex, vmax=20, age_weight='L'):
         age_list.append(np.delete(adiff,exarr))
         metal_list.append(np.delete(mdiff,exarr))
 
-        rphi = '{}/NGC_891_P{}_bin30_rphi.dat'.format(basedir,p+1)
-        rr = np.loadtxt(rphi, usecols=(1,), unpack=True)
-        rlist.append(np.delete(rr,exarr))
+        if height:
+            loc = '{}/NGC_891_P{}_bin30_locations.dat'.format(basedir,p+1)
+            z = np.loadtxt(loc, usecols=(5,), unpack=True)
+            clabel = r'$|z|$'
+            rlist.append(np.delete(z, exarr))
+        else:
+            rphi = '{}/NGC_891_P{}_bin30_rphi.dat'.format(basedir,p+1)
+            rr = np.loadtxt(rphi, usecols=(1,), unpack=True)
+            clabel = r'$r$'
+            rlist.append(np.delete(rr,exarr))
         
     age = np.hstack(age_list)
     metal = np.hstack(metal_list)
@@ -218,11 +232,115 @@ def mass_diff(output, exclude=pap_ex, vmax=20, age_weight='L'):
     r = np.hstack(rlist)
     scat = ax.scatter(age,metal,c=r,s=45,linewidth=0, cmap=plt.cm.gnuplot2, vmin=0, vmax=vmax)
     cb = fig.colorbar(scat)
-    cb.set_label(r'$r$')
+    cb.set_label(clabel)
 
     pp = PDF(output)
     pp.savefig(fig)
     pp.close()
     plt.close(fig)
 
+    return
+
+def MC_contour(output,zlims=[0,0.4],bins=30):
+
+    basedir = '/d/monk/eigenbrot/WIYN/14B-0456/anal/model_balmer/bc03/multi_Z/MLWA_uncertainty/MC/top4'
+
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111)
+    data1_list = []
+    data2_list = []
+
+    for p in [1,2,3,4,5,6]:
+        loc = '{}/NGC_891_P{}_bin30_locations.dat'.format(basedir,p)
+        z = np.loadtxt(loc, usecols=(5,), unpack=True)
+        z = np.abs(z)
+        idx = np.where((z >= zlims[0]) & (z < zlims[1]))[0]
+        N = 1
+        while True:
+            try:
+                coef = '{}/MCdir/NGC_891_P{:}_bin30_allz2.MC{:03n}.fits'.format(basedir,p,N)
+                c = pyfits.open(coef)[1].data
+            except IOError:
+                break
+
+            if N-1 in idx:
+                print coef, z[N-1]
+                p1 = c['MLWA']
+                p2 = c['MLWZ'] - np.mean(c['MLWZ'])
+                data1_list.append(p1)
+                data2_list.append(p2)
+            
+            N += 1 
+
+    data1 = np.hstack(data1_list)
+    data2 = np.hstack(data2_list)
+
+    H1, xe1, ye1 = np.histogram2d(data1, data2, bins=bins)
+    x1 = 0.5*(xe1[:-1] + xe1[1:])
+    y1 = 0.5*(ye1[:-1] + ye1[1:])
+
+    H1 /= np.max(H1)
+
+    levels = [0.005,0.1, 0.4, 0.7, 0.9]
+    ax1.contour(x1,y1,H1.T,levels,colors='k')
+    
+    ax1.set_xlabel(r'$\tau_L$')
+    ax1.set_ylabel(r'$Z_L - \left<Z_L\right>$')
+
+    pp = PDF(output)
+    pp.savefig(fig)
+    pp.close()
+    
+    return
+
+def MC_scatter(output,zlims=[0,0.4],bins=30):
+
+    basedir = '/d/monk/eigenbrot/WIYN/14B-0456/anal/model_balmer/bc03/multi_Z/MLWA_uncertainty/MC/top4'
+
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111)
+    data1_list = []
+    data2_list = []
+    z_list = []
+
+    for p in [1,2,3,4,5,6]:
+        loc = '{}/NGC_891_P{}_bin30_locations.dat'.format(basedir,p)
+        z = np.loadtxt(loc, usecols=(5,), unpack=True)
+        z = np.abs(z)
+        idx = np.where((z >= zlims[0]) & (z < zlims[1]))[0]
+        N = 1
+        while True:
+            try:
+                coef = '{}/MCdir/NGC_891_P{:}_bin30_allz2.MC{:03n}.fits'.format(basedir,p,N)
+                c = pyfits.open(coef)[1].data
+            except IOError:
+                break
+
+            if N-1 in idx:
+                print coef, z[N-1]
+                p1 = c['MLWA']
+                p2 = c['MLWZ'] - np.mean(c['MLWZ'])
+                data1_list.append(p1)
+                data2_list.append(p2)
+                z_list.append([z[N-1]]*p1.size)
+            
+            N += 1 
+
+    data1 = np.hstack(data1_list)
+    data2 = np.hstack(data2_list)
+    zd = np.hstack(z_list)
+
+    print data1.shape
+
+    scat = ax1.scatter(data1, data2, c=zd, s=2,alpha=0.4,linewidth=0, cmap=plt.cm.gnuplot, vmin=0, vmax=3)
+    
+    ax1.set_xlabel(r'$\tau_L$')
+    ax1.set_ylabel(r'$Z_L - \left<Z_L\right>$')
+    cb = fig.colorbar(scat)
+    cb.set_label(r'$|z|$')
+
+    pp = PDF(output)
+    pp.savefig(fig)
+    pp.close()
+    
     return
