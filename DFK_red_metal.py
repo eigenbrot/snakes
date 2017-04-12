94ef0e4cfcf51644fb9a4516dfe6d0d60cea9e99
eigenbrot <eigenbrot@astro.wisc.edu>
2017-04-12 17:42:28 -0600

    Fuck it, these changes were made so long ago I have no idea why I did it. Sorry.
    At least I now have a clean directory

--- DFK_red_metal.py
+++ DFK_red_metal.py
@@ -8,6 +8,7 @@ from matplotlib.backends.backend_pdf import PdfPages as PDF
 def main(model_name='DFK_allZ_vardisp.fits', output='test.pdf'):
 
     models = pyfits.open(model_name)[1].data[0]
+    fididx = [0,0,3,3]
     clist = ['#377eb8','#4daf4a','#984ea3','#e41a1c']
     Zlist = [0.2, 0.4, 1, 2.5]
     pp = PDF(output)
@@ -19,7 +20,7 @@ def main(model_name='DFK_allZ_vardisp.fits', output='test.pdf'):
         #take out 2 lowest metallicities
         monoage = monoage[2:,:]
 
-        best_pars = do_fits(monoage, models['WAVE']).T
+        best_pars = do_fits(monoage, models['WAVE'], fididx[i]).T
         best_taus = best_pars[0]
         best_norms = best_pars[1]
         # best_taus = np.zeros(3,2) + 5
@@ -30,18 +31,31 @@ def main(model_name='DFK_allZ_vardisp.fits', output='test.pdf'):
         ax.set_xlabel('Wavelength')
         ax.set_ylabel('Normalized flux')
         redd = best_norms[:,None]*np.exp(-1 * best_taus[:,None] * (models['WAVE'][None,:]/5500.)**(-0.7))
+        # redd = np.zeros((4, tmpredd.shape[1]))
+        # kk = 0
+        # for k in range(4):
+        #     if k == fididx[i]:
+        #         continue
+        #     else:
+        #         redd[k] = tmpredd[kk]
+        #         kk += 1
         print redd.shape
-        print best_norms, np.sum(redd, axis=1)/np.sum(monoage[:-1,:], axis=1)
-        ax.plot(models['WAVE'], monoage[3], color=clist[3], label=Zlist[3], lw=2, alpha=0.5)
-        ax.plot(models['WAVE'], monoage[0]*redd[0], color=clist[0], label=Zlist[0])
-        ax.plot(models['WAVE'], monoage[1]*redd[1], color=clist[1], label=Zlist[1])
-        ax.plot(models['WAVE'], monoage[2]*redd[2], color=clist[2], label=Zlist[2])
-        ax.text(0.5,0.06,'{:5.3f}, {:5.3f}'.format(best_taus[0], best_norms[0]), 
+        print best_norms, np.sum(redd, axis=1)/np.sum(monoage[:,:], axis=1)
+        for j in range(4):
+            if j == fididx[i]:
+                ax.plot(models['WAVE'], monoage[j], color=clist[j], label=Zlist[j], lw=2, alpha=0.5)
+            else:
+                ax.plot(models['WAVE'], monoage[j]*redd[j], color=clist[j], label=Zlist[j])
+        # ax.plot(models['WAVE'], monoage[1]*redd[1], color=clist[1], label=Zlist[1])
+        # ax.plot(models['WAVE'], monoage[2]*redd[2], color=clist[2], label=Zlist[2])
+        ax.text(0.5,0.095,'{:5.3f}, {:5.3f}'.format(best_taus[0], best_norms[0]), 
                 color=clist[0], transform=ax.transAxes, fontsize=9)
-        ax.text(0.5,0.035,'{:5.3f}, {:5.3f}'.format(best_taus[1], best_norms[1]), 
+        ax.text(0.5,0.06,'{:5.3f}, {:5.3f}'.format(best_taus[1], best_norms[1]), 
                 color=clist[1], transform=ax.transAxes, fontsize=9)
-        ax.text(0.5,0.01,'{:5.3f}, {:5.3f}'.format(best_taus[2], best_norms[2]), 
+        ax.text(0.5,0.035,'{:5.3f}, {:5.3f}'.format(best_taus[2], best_norms[2]), 
                 color=clist[2], transform=ax.transAxes, fontsize=9)
+        ax.text(0.5,0.01,'{:5.3f}, {:5.3f}'.format(best_taus[3], best_norms[3]), 
+                color=clist[3], transform=ax.transAxes, fontsize=9)
         ax.text(0.3, 0.035, r'$\tau_V$, normalization', transform=ax.transAxes, fontsize=9)
         
         ax.legend(loc=0, title=r'$Z/Z_\odot$')
@@ -52,13 +66,12 @@ def main(model_name='DFK_allZ_vardisp.fits', output='test.pdf'):
     pp.close()
     return
 
-def do_fits(monoage, wave):
+def do_fits(monoage, wave, fididx):
 
-    #Assume the last one is the highest metallicity
-    fiducial = monoage[3]
+    fiducial = monoage[fididx]
 
-    results = np.zeros((3,2))
-    for i in range(3):
+    results = np.zeros((4,2))
+    for i in range(4):
         p0 = [1.0,1.0]
         pf = spo.fmin(red_func, p0, args=(monoage[i], fiducial, wave))
         results[i] = np.array(pf)
